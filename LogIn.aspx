b8f601de88c14196fec2fd30e35de84409e85a6e
TechWebDots <49743764+nitin27verma@users.noreply.github.com>
2021-02-06 22:53:12 +0530

    Add LogIn Design

---
+++ LogIn.aspx
@@ -0,0 +1,44 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="LogIn.aspx.cs" Inherits="DotNetConcept.LogIn" %>
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head id="Head1" runat="server">
+    <title></title>
+    <script src="../Scripts/jquery-3.4.1.min.js"></script>
+    <link href="Styles/bootstrap-3.3.7.css" rel="stylesheet" />
+    <link href="Styles/Site.css" rel="stylesheet" />
+</head>
+<body>
+    <form id="frmLogIn" class="frmMaster" runat="server">
+        <table class="form-control welcomeProjectText" style="padding: 6px 6px;height: auto;">
+            <tbody>
+                <tr>
+                    <td class="panelTD welcomeProjectTextTD">TechWebDots<br />LogIn Design<br/>(Bootstrap and CSS)</td>
+                    <td class="panelTDControl" style="width: 30%;">
+                        <asp:Label ID="lblVersionInfo" style="float: right;font-size:9px;" runat="server" Text=""></asp:Label>
+                        <table id="tblLogIn" class="table tblLogIn">
+                            <tbody>
+                                <tr><td><asp:Label ID="lblUserName" CssClass="form-control panelControl controlBackground" runat="server" Text="User Id"></asp:Label></td></tr>
+                                <tr><td><asp:TextBox placeholder="Enter User Id" CssClass="form-control panelControl" ClientIDMode="Static" ID="txtUserId" runat="server"></asp:TextBox></td></tr>
+                                <tr><td><asp:Label ID="lblPassword" CssClass="form-control panelControl controlBackground" runat="server" Text="Password"></asp:Label></td></tr>
+                                <tr><td><asp:TextBox placeholder="Enter Password" CssClass="form-control panelControl" ClientIDMode="Static" ID="txtPassword" runat="server" TextMode="Password"></asp:TextBox></td></tr>
+                                <tr><td><asp:CheckBox ID="chkBoxRememberMe" runat="server" Text="Remember me" /></td></tr>
+                                <tr><td><asp:Label ID="lblErrorMessage" style="color: red;" runat="server" Text=""></asp:Label></td></tr>
+                                <tr><td><asp:Button ID="btnLogIn" CssClass="btn btn-default" runat="server" Text="LogIn" OnClick="btnLogIn_Click" OnClientClick="return AuthenticateUser();"/></td></tr>
+                                <tr><td><a href="mailto:techwebdots@gmail.com?Subject=Request:Reset%20Password" target="_top">Forgot Password?</a></td></tr>
+                            </tbody>
+                        </table>
+                    </td>
+                </tr>
+                <tr>
+                    <td class="panelTD controlBackground" style="text-align: center;">
+                        Don't have an account? <a href="mailto:techwebdots@gmail.com?Subject=Request:New%20Account" style="color: white !important;" target="_top">Click here to Contact</a>
+                    </td>
+                    <td class="panelTDControl"></td>
+                </tr>                
+            </tbody>
+        </table>
+        <div style="text-align: center;">
+            <p>Copyright © TechWebDots, All Rights Reserverd - <%: DateTime.Now.Year %></p>
+        </div>
+    </form>   
+</body>
+</html>
---
+++ LogIn.aspx.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.Reflection;
+using System.Web.Security;
+using System.Configuration;
+using System.Data;
+using Newtonsoft.Json;
+using System.Net.Http;
+using System.Net.Http.Headers;
+
+namespace DotNetConcept
+{
+    public partial class LogIn : System.Web.UI.Page
+    {
+        Boolean IsAuthenticated = false;
+        HttpClient WebAPIServiceClient = null;
+        HttpResponseMessage response;
+        string jsonString = string.Empty;
+        DataTable _dt;
+        public string UserId
+        {
+            get
+            {
+                return txtUserId.Text;
+            }
+            set
+            {
+                txtUserId.Text = value;
+            }
+        }
+        public string Password
+        {
+            get
+            {
+                return txtPassword.Text;
+            }
+            set
+            {
+                txtPassword.Text = value;
+            }
+        }
+        public bool RememberMe
+        {
+            get
+            {
+                return chkBoxRememberMe.Checked;
+            }
+            set
+            {
+                chkBoxRememberMe.Checked = value;
+            }
+        }
+        public string Message
+        {
+            get
+            {
+                return lblErrorMessage.Text;
+            }
+            set
+            {
+                lblErrorMessage.Text = value;
+            }
+        }
+        protected void Page_Load(object sender, EventArgs e)
+        {            
+            lblVersionInfo.Text = "Version :" + Assembly.GetExecutingAssembly().GetName().Version.ToString();
+            if (!IsAuthenticated)
+            {
+                //your own action item
+            }
+        }      
+        protected void btnLogIn_Click(object sender, EventArgs e)
+        {
+            AuthenticateUser();
+        }
+        public void AuthenticateUser()
+        {
+            if (!string.IsNullOrEmpty(UserId.Trim()) || !string.IsNullOrEmpty(Password.Trim()))
+            {
+                if (ValidateId(UserId))
+                {
+                    Message = "LoggingIn...";
+                    FormsAuthentication.RedirectFromLoginPage(UserId, RememberMe);
+                }
+                else
+                {
+                    Message = "LogInErrorInvalidCredential";
+                }
+            }
+            else
+            {
+                Message = "LogInErrorInvalidCredential";
+            }
+        }        
+        public bool ValidateId(string UserId)
+        {
+            bool IsValid;            
+            #region Consume GET method of webAPI
+            using (WebAPIServiceClient = new HttpClient())
+            {
+                WebAPIServiceClient.BaseAddress = new Uri(ConfigurationManager.AppSettings["WebAPIServiceUrl"].ToString());
+                WebAPIServiceClient.DefaultRequestHeaders.Accept.Clear();
+                WebAPIServiceClient.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+                response = WebAPIServiceClient.GetAsync("api/values/GetValidateID?id=" + UserId).Result;
+                if (response.IsSuccessStatusCode)
+                {
+                    jsonString = response.Content.ReadAsStringAsync().Result;
+                    _dt = GetUserData(UserId);
+                }
+                else
+                {
+                    Message = "Internal server Error with API";
+                }
+            }
+            #endregion
+            return bool.TryParse(jsonString, out IsValid);
+        }        
+        public DataTable GetUserData(string id)
+        {
+            using (WebAPIServiceClient = new HttpClient())
+            {
+                #region Consume GET method
+                WebAPIServiceClient.BaseAddress = new Uri(ConfigurationManager.AppSettings["WebAPIServiceUrl"].ToString());
+                WebAPIServiceClient.DefaultRequestHeaders.Accept.Clear();
+                WebAPIServiceClient.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+                response = WebAPIServiceClient.GetAsync("api/values/GetUserData?id=" + id).Result;
+                if (response.IsSuccessStatusCode)
+                {
+                    jsonString = response.Content.ReadAsStringAsync().Result;
+                    _dt=(DataTable)JsonConvert.DeserializeObject(jsonString,(typeof(DataTable)));
+                }
+                else
+                {
+                    Message = "Internal server Error with API";
+                }
+            }           
+            #endregion
+            return _dt;
+        }        
+    }
+}
